25087dfa842d643d716e00f1f5566bb38cc937a4
potsane <potsanemohale@gmail.com>
2024-06-25 18:53:23 +0200

    Add rich tech widget

---
+++ lib/ui/widgets/two_part_rich_text.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../theme/app_pallete.dart';
+
+class TwoPartRichText extends StatelessWidget {
+  final String partOne;
+  final String partTwo;
+  final void Function() onClick;
+
+  const TwoPartRichText({
+    super.key,
+    required this.partOne,
+    required this.partTwo,
+    required this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RichText(
+          text: TextSpan(
+            text: partOne,
+            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  fontWeight: FontWeight.normal,
+                  color: AppPallete.lightBlue700,
+                ),
+            children: <TextSpan>[
+              TextSpan(
+                text: " $partTwo",
+                style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                      fontWeight: FontWeight.normal,
+                      color: AppPallete.lightBlue300,
+                    ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
