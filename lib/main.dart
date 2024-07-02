dd12cdf6a0dfd1168d362b1dee5afb6f4266cc20
potsane <potsanemohale@gmail.com>
2024-07-02 18:38:53 +0200

    Add dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,17 @@
-import 'package:blogs/common/keys/app_keys.dart';
+import 'package:blogs/injection/init_dependancies.dart';
 import 'package:blogs/ui/bloc/auth_bloc.dart';
 import 'package:blogs/ui/login/login.dart';
 import 'package:blogs/ui/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-import 'data/local/auth_remote_data_source.dart';
-import 'data/repository/auth_repository_impl.dart';
-import 'domain/usecases/user_sign_up.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(
-    url: AppKeys.supabaseUrl,
-    anonKey: AppKeys.supabaseAnonKey,
-  );
+  await initDependencies();
   runApp(
     MultiBlocProvider(
       providers: [
-        BlocProvider(
-          create: (_) => AuthBloc(
-            userSignUp: UserSignUp(
-              repository: AuthRepositoryImpl(
-                remoteDataSource: AuthRemoteDataSourceImpl(supabase: supabase),
-              ),
-            ),
-          ),
-        )
+        BlocProvider(create: (_) => serviceLocator<AuthBloc>()),
       ],
       child: const MyApp(),
     ),
